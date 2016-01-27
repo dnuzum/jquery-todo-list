ab6020d22735bb36b2fddeb1147954a365b12c88
dnuzum <derek.nuzum@gmail.com>
2016-01-26 22:52:07 -0800

    dnuzum todo list

---
+++ js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	$('span').on('click', function() {
+		// alert('clicked');
+	});
+// OR
+	// $('span').click(function() {
+	// 	alert('clicked');
+	// });
+
+
+	$('#to-do').on('submit', function(e) {
+		e.preventDefault();
+		var $todo = $('#to-do input').val();
+		console.log($todo);
+		$('#to-do input').val('');
+
+		$('ul').append('<li>' + $todo + '<span> -X</span></li>');
+ });
+
+  $('ul').on('click', 'li span', function() {
+    console.log('Item clicked!');
+    $(this).parent().addClass("strike");
+  });
+
+  $('ul').on('click', 'li span', function() {
+  console.log('Item clicked!');
+  $(this).parent().addClass("strike");
+// $(this).parent().toggleClass("strike");
+  });
+
+});
