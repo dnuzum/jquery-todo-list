0cd5f8bca61078d94f1d3165d0c9de6bcb5285ae
dnuzum <derek.nuzum@gmail.com>
2016-01-27 10:49:58 -0800

    added jquery ui for sortable. Tried to get sweetAlert to work. No dice.

--- js/script.js
+++ js/script.js
@@ -1,33 +1,31 @@
 $(document).ready(function() {
-	$('span').on('click', function() {
+	// $('span').on('click', function() {
 		// alert('clicked');
-	});
+	// });
 // OR
 	// $('span').click(function() {
 	// 	alert('clicked');
 	// });
-
-
 	$('#to-do').on('submit', function(e) {
 		e.preventDefault();
 		var $todo = $('#to-do input').val();
-		console.log($todo);
+		// console.log($todo);
 		$('#to-do input').val('');
 
-		$('ul').append('<li>' + $todo + '<span> -X</span></li>');
- });
-
+		$('ul').append('<li>' + $todo + '<span> -X</span></li>');	
+ 	});
+	$('ul').sortable();
   // $('ul').on('click', 'li span', function() {
   //   console.log('Item clicked!');
   //   $(this).parent().addClass("strike");
   // });
 	  $('ul').css('cursor', 'pointer');
 	  $('ul').on('click', 'li', function() {
-	  console.log('li clicked!');
+	  // console.log('li clicked!');
 	  $(this).toggleClass("strike");
 	});
 	  $('ul').on('click', 'li span', function() {
-	  console.log('span clicked!');
+	  // console.log('span clicked!');
 	  $(this).parent().remove();
   });
 });
